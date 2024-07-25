ff195b77c03a5f252e2806426c41494387bfd94f
Devendra <devendrav1910@gmail.com>
2024-07-25 15:58:07 +0530

    delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const {z} = require("zod")
-
-function validation(arr){
-  const schema = z.array(z.number());
-  const resp = schema.safeParse(arr)
-  console.log(resp);
-}
-
-validation([2,1,4,3])
